b15c22cf56c0d2a1b0aad9cf9aaa655855ba3ecd
renzky15 <renzowensantillan@gmail.com>
2020-11-02 22:38:41 +0800

    Updates

---
+++ precache-manifest.6a710a6bc9416d1eb5440512b3e70d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b4fb2de2870b18b3e8b3805f575917",
+    "url": "/wifi-tambayan-v2/index.html"
+  },
+  {
+    "revision": "64650f50e989ad55695f",
+    "url": "/wifi-tambayan-v2/static/css/main.cf9fdac6.chunk.css"
+  },
+  {
+    "revision": "058fdb914da5cd49bd45",
+    "url": "/wifi-tambayan-v2/static/js/2.00303ce4.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/wifi-tambayan-v2/static/js/2.00303ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64650f50e989ad55695f",
+    "url": "/wifi-tambayan-v2/static/js/main.0ad9a976.chunk.js"
+  },
+  {
+    "revision": "13f574605f6cc1350790",
+    "url": "/wifi-tambayan-v2/static/js/runtime-main.43c43562.js"
+  }
+]);
